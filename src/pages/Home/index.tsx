86eb5eb7df46ccaabb40b2eb8a17d7e65323b07b
Guilherme <guilherme@alume.com>
2023-09-04 22:16:24 -0300

    create hero section

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,59 @@
+import { Layout } from '@components'
+import { Benefits, Hero } from './styles'
+import heroImg from '../../assets/images/hero.png'
+import { Coffee, Package, ShoppingCart, Timer } from '@phosphor-icons/react'
+import { useTheme } from 'styled-components'
+
 export function Home() {
-  return <h1>Home</h1>
+  const theme = useTheme()
+
+  return (
+    <Layout>
+      <Hero>
+        <div className="left">
+          <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+          <p className="text-lg">
+            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+            hora
+          </p>
+
+          <Benefits>
+            <div className="item">
+              <div className="icon" style={{ background: theme['yellow-800'] }}>
+                <ShoppingCart size={16} weight="fill" />
+              </div>
+              Compra simples e segura
+            </div>
+            <div className="item">
+              <div className="icon" style={{ background: theme['base-700'] }}>
+                <Package size={16} weight="fill" />
+              </div>
+              Embalagem mantém o café intacto
+            </div>
+            <div className="item">
+              <div className="icon" style={{ background: theme['yellow-500'] }}>
+                <Timer size={16} weight="fill" />
+              </div>
+              Entrega rápida e rastreada
+            </div>
+            <div className="item">
+              <div className="icon" style={{ background: theme['purple-500'] }}>
+                <Coffee size={16} weight="fill" />
+              </div>
+              O café chega fresquinho até você
+            </div>
+          </Benefits>
+        </div>
+
+        <div className="right">
+          <img
+            className="img-fluid"
+            src={heroImg}
+            style={{ maxWidth: 476 }}
+            alt="Imagem de um copo de café com a embalagem da coffee delivery"
+          />
+        </div>
+      </Hero>
+    </Layout>
+  )
 }
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,37 @@
+import { styled } from 'styled-components'
+
+export const Hero = styled.section`
+  display: flex;
+  gap: 32px;
+
+  & > .left {
+  }
+
+  & > .right {
+    flex-shrink: 0;
+  }
+`
+
+export const Benefits = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  row-gap: 24px;
+  margin-top: 64px;
+
+  .item {
+    display: flex;
+    align-items: center;
+    gap: 12px;
+  }
+
+  .icon {
+    width: 32px;
+    height: 32px;
+    padding: 8px;
+    border-radius: 9999px;
+
+    & > svg {
+      color: #fff;
+    }
+  }
+`
