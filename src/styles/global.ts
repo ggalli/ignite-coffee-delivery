6335c025c5afb3c08269e0e0aba4755ca50ef635
Guilherme <guilherme@alume.com>
2023-09-04 22:16:00 -0300

    create new global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -21,5 +21,36 @@ export const GlobalStyle = createGlobalStyle`
     font-family: 'Roboto', sans-serif;
     font-weight: 400;
     font-size: 1rem;
+    line-height: 130%;
+  }
+
+  section {
+    padding: 88px 0;
+  }
+
+  h1, h2, h3, h4, h5 {
+    font-family: 'Baloo 2', sans-serif;
+    font-weight: 800;
+    line-height: 130%;
+  }
+
+  h1 { 
+    font-size: 3rem; 
+    color: ${(props) => props.theme['base-900']}; 
+    margin-bottom: 16px;
+  }
+  h2 { font-size: 2rem }
+  h3 { font-size: 1.5rem }
+  h4 { font-size: 1.25rem; font-weight: 700; }
+  h5 { font-size: 1.125rem; font-weight: 700; }
+
+  .text-lg { font-size: 1.25rem }
+  .text-md { font-size: 1rem }
+  .text-sm { font-size: 0.875rem }
+  .text-xs { font-size: 0.75rem }
+
+  .img-fluid {
+    width: 100%;
+    height: auto;
   }
 `
