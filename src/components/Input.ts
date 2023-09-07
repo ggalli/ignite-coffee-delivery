aa462d9de1bd1f524759657e15ec778fe9ce9dc6
Guilherme <guilherme@alume.com>
2023-09-06 22:51:31 -0300

    add input stretch prop for full width

--- src/components/Input.ts
+++ src/components/Input.ts
@@ -1,6 +1,12 @@
 import { styled } from 'styled-components'
 
-export const Input = styled.input`
+interface InputProps {
+  stretch?: boolean
+}
+
+export const Input = styled('input').withConfig({
+  shouldForwardProp: (prop) => !['stretch'].includes(prop),
+})<InputProps>`
   background-color: ${({ theme }) => theme['base-200']};
   color: ${({ theme }) => theme['base-700']};
   padding: 12px;
@@ -10,6 +16,8 @@ export const Input = styled.input`
   font-size: 14px;
   font-weight: 400;
   line-height: 130%;
+
+  width: ${({ stretch }) => stretch && '100%'};
 
   &:focus {
     border: ${({ theme }) => `1px solid ${theme['yellow-800']}`};
