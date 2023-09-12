e052c32fe039e33e5cdf096860eceaf98be84b17
Guilherme <guilherme@alume.com>
2023-09-11 22:05:29 -0300

    refactor button component

--- src/components/Button.ts
+++ src/components/Button.ts
@@ -3,27 +3,32 @@ import { styled } from 'styled-components'
 
 type ButtonProps = ComponentProps<'button'> & {
   variant: 'primary' | 'secondary'
+  stretch?: boolean
 }
 
-export const Button = styled.button<ButtonProps>`
+export const Button = styled('button').withConfig({
+  shouldForwardProp: (prop) => !['stretch'].includes(prop),
+})<ButtonProps>`
   all: unset;
   display: flex;
   justify-content: center;
   align-items: center;
-  min-width: 8.25rem;
-  padding: 12px 8px;
+  gap: 4px;
   border-radius: 6px;
   cursor: pointer;
 
   font-size: 0.875rem;
-  font-weight: 700;
-  line-height: 160%;
   text-transform: uppercase;
+
+  ${({ stretch }) => stretch && 'width: 100%; padding: 12px 0'};
 
   ${({ variant, theme }) => {
     switch (variant) {
       case 'primary':
         return `
+        min-width: 132px;
+        padding: 12px 8px;
+        font-weight: 700;
         background-color: ${theme['yellow-500']};
         color: ${theme.white};
         &:hover {
@@ -33,11 +38,16 @@ export const Button = styled.button<ButtonProps>`
 
       case 'secondary':
         return `
+        height: 32px;
+        padding: 0 8px;
         background-color: ${theme['base-300']};
         color: ${theme['base-700']};
         &:hover {
           background-color: ${theme['base-400']};
           color: ${theme['base-800']};
+        }
+        & > svg {
+          color: ${theme['purple-500']};
         }
       `
 
