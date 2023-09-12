8d2c2f27eaa3eba137f03f91e1179094651f886a
Guilherme <guilherme@alume.com>
2023-09-11 22:47:51 -0300

    create success route

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Checkout } from './pages/Checkout'
+import { SuccessPage } from './pages/Success'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/checkout" element={<Checkout />} />
+      <Route path="/success" element={<SuccessPage />} />
     </Routes>
   )
 }
